06a28a0e484459110ff93014d8861bf592c505aa
bader <bader.boshnak@e.braude.ac.il>
2025-11-07 16:23:20 +0200

    yhj

--- server.js
+++ server.js
@@ -20,26 +20,30 @@ const transporter = nodemailer.createTransport({
 app.post("/api/contact", async (req, res) => {
   console.log("fh",req.body)
   const { name, email, phone, projectType, message } = req.body;
-
+  console.log("fh4",req.body)
   if (!name || !email || !message) {
+    console.log("fh6",req.body)
     return res.status(400).json({ error: "Missing required fields" });
   }
-
+console.log("fh1",req.body)
   const mailOptions = {
+    
     from: `"Luxury Tech Contact" <${process.env.GMAIL_USER}>`,
     to: process.env.MAIL_TO,
     subject: `New Inquiry from ${name}`,
     text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nProject Type: ${projectType}\n\nMessage:\n${message}`,
   };
-
+console.log("fh2",req.body)
   try {
+    console.log("fh8",req.body)
     await transporter.sendMail(mailOptions);
     res.json({ success: true });
   } catch (error) {
+    console.log("fh9",req.body)
     console.error(error);
     res.status(500).json({ success: false, error: "Failed to send email" });
   }
 });
-
+console.log("fh3",req.body)
 const PORT = process.env.PORT || 5175;
 app.listen(PORT, "0.0.0.0", () => console.log(`Server running on http://localhost:${PORT}`));
