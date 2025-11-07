68b31cb7f51992aa133de477780418e7b3454f3a
bader <bader.boshnak@e.braude.ac.il>
2025-11-07 15:45:08 +0200

    thhg

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.post("/api/contact", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5175;
-app.listen(port, "0.0.0.0", () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on http://localhost:${PORT}`));
